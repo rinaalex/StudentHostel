8f484ff085adb77b72638d81a2811d501b676821
Katherine Burova <katherineburova@gmail.com>
2019-02-16 15:28:23 +0300

    Добавлен проект для Unit-тестирования

---
+++ StudentHostel/UnitTestStudentHostel/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestStudentHostel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestStudentHostel")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("951e5caa-885e-4170-b39c-15f85519b6f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StudentHostel/UnitTestStudentHostel/UnitTestGroupsListViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentHostelApp.Model;
+using StudentHostelApp.ViewModel;
+using StudentHostelApp.DataAccess;
+
+namespace UnitTestStudentHostel
+{
+    [TestClass]
+    public class UnitTestGroupsListViewModel
+    {
+        [TestMethod]
+        public void GetAllGroups()
+        {
+            var data = new List<Group>
+            {
+                new Group {GroupId=1, GroupName="TestGroup", SoftDeleted=false},
+                new Group{GroupId=2, GroupName="AnotherTestGroup", SoftDeleted=false}
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Group>>();
+            mockSet.As<IQueryable<Group>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Group>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Group>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Group>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<StudentHostelContext>();
+            mockContext.Setup(c => c.Groups).Returns(mockSet.Object);
+            var viewModel = new GroupListViewModel(mockContext.Object);
+            var groups = viewModel.GroupList;
+
+            Assert.AreEqual(2, groups.Count);
+            Assert.AreEqual("TestGroup", groups[0].GroupName);
+            Assert.AreEqual("AnotherTestGroup", groups[1].GroupName);            
+        }
+    }
+}
